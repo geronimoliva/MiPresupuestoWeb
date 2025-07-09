7caeffd543889bb24c1f105b94d7cc527540627b
geronimoliva <49896221+geronimoliva@users.noreply.github.com>
2025-07-09 13:51:01 -0300

    Update monto.py
    agregue la funcion de numero a letra con num2words

--- monto.py
+++ monto.py
@@ -1,3 +1,5 @@
+from num2words import num2words
+
 def formatear_numero(valor):
     try:
         valor_float = float(valor)
@@ -7,3 +9,17 @@ def formatear_numero(valor):
         return numero_formateado
     except ValueError:
         return "Error: Ingresa un número válido"
+    
+
+
+def monto_a_letras(monto):
+    entero = int(float(monto))
+    centavos = int(round((float(monto) - entero) * 100))
+    
+    texto_entero = num2words(entero, lang='es')
+    if centavos > 0:
+        texto_centavos = num2words(centavos, lang='es')
+        resultado = f"{texto_entero} con {texto_centavos} centavos"
+    else:
+        resultado = f"{texto_entero} con cero centavos"
+    return resultado.lower()
